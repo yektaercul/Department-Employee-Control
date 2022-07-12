138b35c164da8c7065df45cd4f12115fdaccdde4
yektaercul <75091412+yektaercul@users.noreply.github.com>
2022-07-12 20:23:33 +0300

    Today's last commit

--- src/main/java/yekta/springtest/controller/EmployeeController.java
+++ src/main/java/yekta/springtest/controller/EmployeeController.java
@@ -1,13 +1,27 @@
 package yekta.springtest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import yekta.springtest.model.Employee;
 
 //@Controller
 @RestController // this combines @Controller and @ResponseBody
 public class EmployeeController {
 
+    @Value("${app.name}")  // application.properties dosyasının içindeki
+                            // bilgileri bu sayede okuyabilirsin
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+
+    @GetMapping("/version")
+    public String getAppDetails(){
+        return appName + " - " + appVersion;
+    }
 
 //    @RequestMapping(value = "/employees", method = RequestMethod.GET)
 //    @ResponseBody
@@ -22,6 +36,18 @@ public class EmployeeController {
     public String getEmployee (@PathVariable("id") Long id) {
         return " Fetching the details for the employee with id " + id;
     }
+
+    @PostMapping("/employees")
+    public String saveEmployee(@RequestBody Employee employee){
+        return "saving the employee details to the database" + employee;
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee updateEmployee (@PathVariable Long id, @RequestBody Employee employee) {
+        System.out.println("updating the employee data for the id " + id);
+        return employee;
+    }
+
 
     //localhost:8080/employees?id=34   id yi burdan alıp RequestParam a yazıyor
     @DeleteMapping("/employees")
--- src/main/java/yekta/springtest/model/Employee.java
+++ src/main/java/yekta/springtest/model/Employee.java
@@ -1,4 +1,19 @@
 package yekta.springtest.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class Employee {
+
+    private String name;
+    private Long age;
+    private String location;
+    private String email;
+    private String department;
+
+
 }
